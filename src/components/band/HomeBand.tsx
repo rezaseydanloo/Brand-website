5bc3292f360af5be3cc2480e386c1d753dcfed68
reza seydanloo <rezaseydanloo.83@gmail.com>
2025-02-10 01:24:38 +0330

    added nike band

---
+++ src/components/band/HomeBand.tsx
@@ -0,0 +1,10 @@
+import NikeIcon from "../navbar/NikeIcon";
+
+export default function HomeBand(){
+
+    return(
+        <div className=" w-full h-8 bg-red-800 mt-3 flex items-center justify-center">
+            <NikeIcon />
+        </div>
+    )
+}   
